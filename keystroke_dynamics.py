7f4aca237593c70e48132fe12dac4c70e508f449
goncalopp <goncalopp@pekkala>
2015-04-26 00:39:14 +0100

    refactor things into proper OOP

--- keystroke_dynamics.py
+++ keystroke_dynamics.py
@@ -4,8 +4,50 @@ import pickle
 from numpy import mean,std
 from scipy.stats import norm
 import Gnuplot, Gnuplot.funcutils
+from abc import ABCMeta, abstractmethod
 
-class TimingExtractor():
+class KeypressEventReceiver(object):
+    __metaclass__=ABCMeta
+    KEY_DOWN, KEY_UP= 0, 1
+    
+    @abstractmethod
+    def on_key(self, key, event_type, time_ms):
+        '''key is a integer
+        event_type is in (KEY_DOWN, KEY_UP)
+        time_ms is the time when the key was (de/)pressed
+        '''
+        pass
+
+class VersionedSerializableClass( object ):
+    __metaclass__=ABCMeta
+    FILE_EXTENSION=".pickle"
+    CLASS_VERSION= -1
+
+    def __init__(self, *args, **kwargs):
+        self._class_version= self.CLASS_VERSION
+    
+    def save_to_file(self, filename):
+        f= open(filename+self.FILE_EXTENSION, 'wb')
+        pickle.dump( self, f)
+        f.close()
+    
+    @classmethod
+    def read_from_file( cls, filename):
+        f=open(filename+self.FILE_EXTENSION, 'rb')
+        data= pickle.load(f)
+        f.close()
+
+        load_error=None
+        if not isinstance( data, cls ):
+            load_error= 'Unexpected instance type'
+        elif data._class_version!=self.CLASS_VERSION:
+            load_error= 'Class version mismatch (expected "{}", got "{}")'.format( self.CLASS_VERSION, data._class_version)
+        if load_error:
+            raise TypeError("Failed to load serialized data from {}: {}".format(filename, load_error))
+
+        return data
+
+class TimingExtractor(KeypressEventReceiver):
     def __init__(self, timing_thresold=500):
         self.pt=0           #last press  time
         self.pk=0           #last pressed key
@@ -19,8 +61,8 @@ class TimingExtractor():
         self.flight_times_after=[[] for i in range(256)]
    
 
-    def keypress(self, key, type, time):    
-        if type==0:
+    def on_key(self, key, type, time):    
+        if type==self.KEY_DOWN:
             flight_time= time - self.pt
             if flight_time<self.timing_thresold:
                 self.flight_times_before[key].append(flight_time)
@@ -29,7 +71,7 @@ class TimingExtractor():
             self.pt=time
             self.pk=key
             
-        if type==1:
+        if type==self.KEY_UP:
             dwell_time= time - self.press_time.pop(key)
             if dwell_time<self.timing_thresold:
                 self.dwell_times[key].append(dwell_time)
@@ -62,40 +104,17 @@ def key_similarity(usersignature, datasignature):
     return norm.cdf(-difference/stddev)
     
 
-def log_key(key, type, time):
-    log.append((key,type, time))
+class KeystrokeCaptureData(KeypressEventReceiver, VersionedSerializableClass):
+    '''Recorded data of actual keystrokes pressed by a user'''
+    FILE_EXTENSION=".keypresses"
+    CLASS_VERSION= 0
     
-def save_log(filename, log):
-    file=open(filename+'.data.pickle', 'wb')
-    pickle.dump(log, file)
-    file.close()
-    
-def open_log(filename):
-    try:
-        file=open(filename+'.data.pickle','rb')
-        log=pickle.load(file)
-        file.close()
-        return log
-    except:
-        return []
+    def __init__(self):
+        self.log= []
 
-def save_signature(filename, signature):
-    file=open(filename+'.signature.pickle', 'wb')
-    pickle.dump(signature, file)
-    file.close()
-
-def load_signature(filename):
-    try:
-        file=open(filename+'.signature.pickle','rb')
-        signature=pickle.load(file)
-        file.close()
-        return signature
-    except:
-        raise Exception('no such signature file')
-    
-
-def callback(key, type, time):
-    log_key(key,type,time)
+    def on_key(self, key, event_type, time_ms):
+        '''Append a keypress event to this capture data'''
+        self.log.append( (key, event_type, time_ms) )
 
 def plot(datalists):
     colour=1
@@ -126,10 +145,16 @@ yuna_signature=load_signature('yuna')
 
 
 mytimingextractor=TimingExtractor(500)
-capture_keys.start(mytimingextractor.keypress)
+
+def testf(*args):
+    print args
+
+#capture_keys.start(mytimingextractor.on_key)
+capture_keys.start(testf)
+
 #log= open_log(filename)
 #for e in log:
-#    mytimingextractor.keypress(e[0],e[1],e[2])
+#    mytimingextractor.on_key(e[0],e[1],e[2])
 
 data=mytimingextractor.dwell_times
 
